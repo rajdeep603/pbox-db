083ed9e922a0eaed0bd89feddb1aae7bfd81fdb8
Rajdeep Shah <rajdeepshah@Rajdeeps-MacBook-Pro.local>
2025-12-20 13:53:16 +0530

    changes

---
+++ db/schema/propertybox/property_inquiry_status.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS propertybox.property_inquiries (
+    record_id SERIAL PRIMARY KEY,
+
+    user_id INT NOT NULL REFERENCES propertybox.users(record_id) ON DELETE CASCADE,
+
+    property_id INT NOT NULL REFERENCES propertybox.properties(record_id) ON DELETE CASCADE,
+
+    property_interest_pl_id INT NOT NULL REFERENCES master.picklist(record_id), -- E.g., Interested, Not Interested, visit done, waiting for approval
+
+    is_active BOOLEAN DEFAULT TRUE,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    inserted_by INT,
+    insert_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_by INT,
+    update_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE IF EXISTS propertybox.property_inquiries
+    ADD COLUMN IF NOT EXISTS contact_number VARCHAR(15),
+    ADD COLUMN IF NOT EXISTS description TEXT;
+
