31d944deb8bcb895527e139dac8622ee12203e07
Asif <iamasifaligarian@gmail.com>
2026-02-20 18:44:02 +0530

    updates

--- db/schema/propertybox/properties.sql
+++ db/schema/propertybox/properties.sql
@@ -43,4 +43,5 @@ ALTER TABLE IF EXISTS propertybox.properties
     ADD COLUMN IF NOT EXISTS monthly_maintenance DECIMAL(10,2),
     ADD COLUMN IF NOT EXISTS availability_from TIMESTAMP,
     ADD COLUMN IF NOT EXISTS property_current_status_id INT REFERENCES master.picklist(record_id),
+    ADD COLUMN IF NOT EXISTS requirement_type_id INT REFERENCES master.picklist(record_id),
     ADD COLUMN IF NOT EXISTS plot_area_sqft DECIMAL(10,2);
