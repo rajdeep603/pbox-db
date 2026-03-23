0b86ae086ce5107bc3dbff287fa3c7a0db8b512f
Asif <iamasifaligarian@gmail.com>
2026-03-23 18:47:08 +0530

    updates

--- db/schema/propertybox/properties.sql
+++ db/schema/propertybox/properties.sql
@@ -51,6 +51,7 @@ ALTER TABLE IF EXISTS propertybox.properties
     ADD COLUMN IF NOT EXISTS flat_floor INT,
     ADD COLUMN IF NOT EXISTS no_of_parkings INT,
     ADD COLUMN IF NOT EXISTS no_of_balconies INT,
+    ADD COLUMN IF NOT EXISTS score INT,
     ALTER COLUMN bhk TYPE VARCHAR(10),
     ALTER COLUMN property_age TYPE VARCHAR(10),
     ADD COLUMN IF NOT EXISTS plot_area_sqft DECIMAL(10,2);
